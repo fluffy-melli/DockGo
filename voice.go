310a4e4302a235b6471c9ae0607cf6bf8310b41e
shibaisdog <kimjaehoon5142@gmail.com>
2025-01-22 01:57:01 +0900

    Update voice.go

--- voice.go
+++ voice.go
@@ -2,13 +2,77 @@ package DockGo
 
 import "github.com/bwmarrin/discordgo"
 
-func (bot *Client) JoinVoiceChannel(guildID, channelID string) error {
-	_, err := bot.Method().ChannelVoiceJoin(guildID, channelID, false, true)
+func (ic *SlashCreate) GetVoiceState() *discordgo.VoiceState {
+	voiceState, err := ic.Client().State.VoiceState(ic.Method().GuildID, ic.Method().Member.User.ID)
+	if err != nil {
+		return nil
+	}
+	return voiceState
+}
+
+func (ic *SlashCreate) GetVoiceChannel() *discordgo.Channel {
+	voiceState := ic.GetVoiceState()
+	if voiceState == nil {
+		return nil
+	}
+	channel, err := ic.Client().State.Channel(voiceState.ChannelID)
+	if err != nil {
+		return nil
+	}
+	return channel
+}
+
+func (ic *SlashCreate) GetVoiceConnection() *discordgo.VoiceConnection {
+	return ic.Client().GetVoiceConnection(ic.Method().GuildID)
+}
+
+func (ic *SlashCreate) JoinVoiceChannel(channelID string) (*discordgo.VoiceConnection, error) {
+	return ic.Client().JoinVoiceChannel(ic.Method().GuildID, channelID)
+}
+
+func (ic *SlashCreate) LeaveVoiceChannel() bool {
+	return ic.Client().LeaveVoiceChannel(ic.Method().GuildID)
+}
+
+func (mc *MessageCreate) GetVoiceState() *discordgo.VoiceState {
+	voiceState, err := mc.Client().State.VoiceState(mc.Method().GuildID, mc.Method().Author.ID)
+	if err != nil {
+		return nil
+	}
+	return voiceState
+}
+
+func (mc *MessageCreate) GetVoiceChannel() *discordgo.Channel {
+	voiceState := mc.GetVoiceState()
+	if voiceState == nil {
+		return nil
+	}
+	channel, err := mc.Client().State.Channel(voiceState.ChannelID)
+	if err != nil {
+		return nil
+	}
+	return channel
+}
+
+func (mc *MessageCreate) GetVoiceConnection() *discordgo.VoiceConnection {
+	return mc.Client().GetVoiceConnection(mc.Method().GuildID)
+}
+
+func (mc *MessageCreate) JoinVoiceChannel(channelID string) (*discordgo.VoiceConnection, error) {
+	return mc.Client().JoinVoiceChannel(mc.Method().GuildID, channelID)
+}
+
+func (mc *MessageCreate) LeaveVoiceChannel() bool {
+	return mc.Client().LeaveVoiceChannel(mc.Method().GuildID)
+}
+
+func (bot *Client) JoinVoiceChannel(guildID, channelID string) (*discordgo.VoiceConnection, error) {
+	vs, err := bot.Method().ChannelVoiceJoin(guildID, channelID, false, true)
 	if err != nil {
 		Print(ERROR, "%v", err)
-		return err
+		return nil, err
 	}
-	return nil
+	return vs, nil
 }
 
 func (bot *Client) LeaveVoiceChannel(guildID string) bool {
